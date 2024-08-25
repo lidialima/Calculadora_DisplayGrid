2d0bd4f017124831663081b350b88d87b7251fcd
lidialima <lidialima2lima@gmail.com>
2024-08-24 21:37:35 -0300

    Projeto DevClub - Calculadora DisplayGrid

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function adicionarCaracter(caracter){
+    const valorInput = document.querySelector('.display').value
+    console.log(valorInput)
+
+    document.querySelector('.display').value = valorInput + caracter
+
+}
+
+function limparTela() {
+    document.querySelector('.display').value = ''
+}
+
+function calcular() {
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = eval(valorInput)
+
+}
+
+function inverte() {
+    const valorInput = document.querySelector('.display').value
+
+    document.querySelector('.display').value = valorInput * -1
+}
